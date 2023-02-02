347ae536b87decebdfece91abb7c693d62616b6f
Omkar Bhostekar <otbhostekar@gmail.com>
2023-02-02 11:15:55 +0530

    lets start

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useSession } from "next-auth/react";
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return <div className="text-black">page</div>;
+};
+
+export default page;
