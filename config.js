9b0656b0a606f8d5d7992ccb3c0e8fc9ed04e569
Archit <architrathod77@gmail.com>
2023-02-02 16:08:21 +0530

    Landing Page

---
+++ config.js
@@ -0,0 +1,33 @@
+const name = "SocialVision";
+const taglines = [
+    "Discover the Truth",
+    "Behind the Screen",
+    "Insight into the Social Network",
+    "Unveiling the Real Picture",
+    "Your Guide to a Transparent Social Web"
+]
+
+const desc = "is a cutting-edge platform for analyzing and understanding the complex landscape of social networks. Our mission is to provide insights into the behavior and interactions of individuals and groups on social media, with a focus on identifying coordinated behavior, toxic speech, and misinformation. Our state-of-the-art tools and data-driven analysis empower users to make informed decisions and navigate the social network with confidence. Join us on our journey to build a better, more transparent social web.";
+
+const features = {
+    "Network Visualization": "A visual representation of the connections between accounts, showing the structure and patterns of the coordinated communities. This could include a graph view, showing the relationships between accounts and highlighting the most active and influential accounts in the network.",
+
+    "Article URL Sharing Analysis": "A tool that allows users to view the frequency and timing of article URL sharing between accounts, helping to identify coordinated behavior and inauthentic activity.",
+
+    "Trend Analysis": "An analysis of the topics and content that the coordinated communities tend to discuss, helping to identify any trends or patterns that may be emerging in the data.",
+
+    "Harm Estimation": "An automated analysis tool that assesses the harm caused by the coordinated behavior, including the promotion of certain hashtags, the targeting of political personalities, the use of hate speech and harassment, and so on.",
+
+    "Activity Monitoring": "A feature that allows users to monitor the activity of specific accounts and communities, providing real - time updates on any coordinated behavior or changes in the network.",
+
+    "Social Media Integration": "The ability to connect the platform to various social media networks, allowing users to analyze data from multiple sources.",
+
+    "Alerts and Notifications": "A system that generates alerts and notifications when specific behaviors or trends are detected, allowing users to quickly respond to any potential issues.",
+    "Historical Data Analysis": "A feature that provides users with access to historical data, allowing them to track changes in the network over time and identify any long - term trends.",
+
+    "Data Export": "The ability to export the data and analysis results in various formats, enabling users to share their findings with others or integrate the data into their own tools and systems.",
+};
+
+export {
+    name, taglines, desc, features
+}
